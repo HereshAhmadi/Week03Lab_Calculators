f4cdede3d05b1822970a8f1e33aeca515199a9a1
699785 <699785@SAIT225170IT.ACDM.DS.SAIT.CA>
2021-01-26 15:13:11 -0700

    Age Calculator BackEnd/finished finished the age calculator by adding the backend to handle the age calculator on number and non number values.

--- src/java/Servlets/AgeCalculatorServlet.java
+++ src/java/Servlets/AgeCalculatorServlet.java
@@ -17,7 +17,26 @@ public class AgeCalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                String message = "";
+        String ageStr = request.getParameter("age");
+        int age;
+        
+        try {
+             age = Integer.parseInt(request.getParameter("age"));
+             message = "Your age next birthday will be " + ++age;
+        } catch (Exception e) {
+            message = "You must enter a number";
+        }
+        
+        
 
+        if (ageStr == null || ageStr.equals("")) {
+            message = "You must give your current age";
+
+        }
+         
+        request.setAttribute("message", message);
+        request.getRequestDispatcher("WEB-INF/ageCalculator.jsp").forward(request, response);
     }
 
 }
